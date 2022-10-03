075c6b56c16a276d4694faedeede9f054f215e5a
Danny K <dxkalnz@gmail.com>
2022-10-03 01:59:40 -0600

    Add fetch request

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import { useEffect } from 'react';
+import './App.css';
+//----NOT SECURE------
+const API_URL = 'http://www.omdbapi.com?apikey=15b1dca3';
+//--------------------
 
+//App Object
 const App = () => {
-	return <h1>App</h1>;
+	//-----Fetch-----
+	const searchMovies = async (title) => {
+		const response = await fetch(`${API_URL}&s=${title}`);
+		const data = await response.json();
+		console.log(data.Search); //JSON results
+	};
+
+	//-----Search function-----
+	useEffect(() => {
+		searchMovies('Watchmen');
+	}, []);
+
+	//-----RETURN-----
+	return () => {
+		<h1>App</h1>;
+	};
 };
 
+//-----export-----
 export default App;
